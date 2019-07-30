1227ef4e41921948e5258f4303e9e7e72a678aee
dpgian <dpgian@gmail.com>
2019-07-30 17:29:41 +0100

    Added faker and two routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,44 @@
+var faker = require('faker')
+
 var appRouter = function (app) {
 
     app.get('/', function(req, res) {
         res.status(200).send('Simple API with Faker.js')
     })
 
+    app.get('/user', function(req, res) {
+        var data = {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            jobTitle: faker.name.jobTitle(),
+            phoneNumber: faker.phone.phoneNumber(),
+            email: faker.internet.email()
+        }
+        res.status(200).send(data)
+    })
+
+    app.get('/users/:num', function(req, res) {
+        var users = []
+        var num = req.params.num
+
+        if(isFinite(num) && num > 0) {
+            for (i = 0; i < num; i++) {
+                users.push({
+                    firstName: faker.name.firstName(),
+                    lastName: faker.name.lastName(),
+                    jobTitle: faker.name.jobTitle(),
+                    phoneNumber: faker.phone.phoneNumber(),
+                    email: faker.internet.email()
+                })
+            }
+        
+        res.status(200).send(users)
+
+        } else {
+            res.status(400).send({ message: 'Enter a valid number' })
+        }
+        
+    })
 }
 
 module.exports = appRouter
